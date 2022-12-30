d4392303bca07f44ee0a0d1137e3d478cd69b495
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2022-12-30 20:20:51 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,140 @@
+const isLeapYear = (year) => {
+    return (
+      (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+      (year % 100 === 0 && year % 400 === 0)
+    );
+  };
+  const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+  };
+  let calendar = document.querySelector('.calendar');
+  const month_names = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  let month_picker = document.querySelector('#month-picker');
+  const search=document.querySelector('.search-btn');
+  console.log(search);
+  // const searchInput=document.querySelector('.data-input');
+  const searchInput=document.getElementById('datainput');
+  console.log(searchInput)
+   searchInput.addEventListener('input',(e)=>{
+    // searchInput.value=searchInput.value.replace(/[^0-9/]/g, "")
+    if(searchInput.value.length === 2){
+      searchInput.value +="/";
+    }
+    if(searchInput.value.length >7){
+      searchInput.value = searchInput.value.slice(0 ,7);
+    }
+    if(e.inputType="deleteContentBackward"){
+      if(searchInput.value.length===3){
+        // searchInput.value=searchInput.value.slice(0,2);
+      }
+    }
+   })
+   search.addEventListener("click",searchs);
+   function searchs(){
+    const searcharr=searchInput.value.split("/");
+    if(searcharr.length==2){
+      if(searcharr[0]>0&&searcharr[0]<13&&searcharr[1].length===4){
+        month= searcharr[0]-1;
+        year=searcharr[1]
+        generateCalendar(month,year);
+        return;
+      }
+    }
+    alert("check date or year")
+   }
+ 
+  month_picker.onclick = () => {
+    month_list.classList.remove('hideonce');
+    month_list.classList.remove('hide');
+    month_list.classList.add('show');
+    
+  };
+  
+  const generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    calendar_days.innerHTML = '';
+    let calendar_header_year = document.querySelector('#year');
+    let days_of_month = [
+      31,
+      getFebDays(year),
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
+    
+    let currentDate = new Date();
+    
+    month_picker.innerHTML = month_names[month];
+    
+    calendar_header_year.innerHTML = year;
+    
+    let first_day = new Date(year, month);
+  
+  
+  for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+  
+      let day = document.createElement('div');
+  
+      if (i >= first_day.getDay()) {
+        day.innerHTML = i - first_day.getDay() + 1;
+
+        if (i - first_day.getDay() + 1 === currentDate.getDate() &&
+          year === currentDate.getFullYear() &&
+          month === currentDate.getMonth()
+        ) {
+          day.classList.add('current-date');
+        }
+      }
+      calendar_days.appendChild(day);
+    }
+  };
+  
+  let month_list = calendar.querySelector('.month-list');
+  month_names.forEach((e, index) => {
+    let month = document.createElement('div');
+    month.innerHTML = `<div>${e}</div>`;
+  
+    month_list.append(month);
+    month.onclick = () => {
+      currentMonth.value = index;
+      generateCalendar(currentMonth.value, currentYear.value);
+      month_list.classList.replace('show', 'hide');
+    };
+  });
+  // iif get last month data
+  (()=> {
+    month_list.classList.add('hideonce');
+  })();
+  document.querySelector('#pre-year').onclick = () => {
+    --currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+  };
+  document.querySelector('#next-year').onclick = () => {
+    ++currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+  };
+  
+  let currentDate = new Date();
+  let currentMonth = { value: currentDate.getMonth() };
+  let currentYear = { value: currentDate.getFullYear() };
+  generateCalendar(currentMonth.value, currentYear.value);
